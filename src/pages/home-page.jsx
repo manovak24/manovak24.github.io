5df6eb6ab24e08dcc6d0a08f542dc3f4cedaaa2a
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-03 12:04:35 -0500

    added react icons to info boxes on home page

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import Hero from '../compontents/hero.jsx';
 import ProjectSlider from '../compontents/slider';
-
 import './home-page.css';
+
+import { BsChatDots, BsClipboardData, BsPeopleCircle } from 'react-icons/bs';
 
 class HomePage extends React.Component {
     render() {
@@ -14,22 +14,29 @@ class HomePage extends React.Component {
                 </div>
                 <div className="home-container"> 
                     <div className="home-text-container">
-                        <p>My name is Mark Novak. I am a web developer based in the north woods of the beuatiful Upper Peninsula of Michigan. Please feel free to check out my About and Projects page to learn more about myself and my projects.</p>
+                        <p>Thank you for checking out my site! I am a web developer based in the north woods of the beuatiful Upper Peninsula of Michigan. Please feel free to check out my About and Projects page to learn more about myself and my projects.</p>
                     </div>
                     
                     <div className="box-container">
                         <div className="info-boxes">
                             <div className="box">
-                                <p>Learn more about myself</p>
-                                <a href="https://manovak24.github.io/about">About</a>
+                                <a href="https://manovak24.github.io/about" className="box-anchor"> 
+                                    <p> Click to learn more about myself</p>
+                                    <BsPeopleCircle size="100" />
+                                </a>
                             </div>
                             <div className="box">
-                                <p>Learn about my project and skills</p>
-                                <a href="https://manovak24.github.io/projects">Projects</a>
+                                <a href="https://manovak24.github.io/projects" className="box-anchor">
+                                    <p>Click to learn about my project and skills</p>
+                                    <BsClipboardData size="100" />
+                                </a> 
                             </div>
                             <div className="box">
-                                <p>Let's connect!</p>
-                                <a href="https://manovak24.github.io/contact">Contact</a>
+                                
+                                <a href="https://manovak24.github.io/contact" className="box-anchor">
+                                    <p>Click here to connect!</p>
+                                    <BsChatDots size="100" />
+                                </a>
                             </div>
                         </div>
                     </div>
