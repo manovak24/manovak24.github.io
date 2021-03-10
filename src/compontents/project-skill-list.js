96b4ef1f35dac1fe7d14f1d871a6fd5394fe51f1
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-10 08:59:11 -0500

    added more skills to project skills component

--- src/compontents/project-skill-list.js
+++ src/compontents/project-skill-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { DiCss3, DiHtml5, DiJavascript, DiReact } from 'react-icons/di';
+import { DiCss3, DiGit, DiGithubBadge, DiHtml5, DiJavascript, DiNodejs, DiNpm, DiReact } from 'react-icons/di';
 
 class ProjectSkillList extends React.Component {
     constructor(props) {
@@ -36,26 +36,50 @@ class ProjectSkillList extends React.Component {
                 {
                     id: 1,
                     title: 'HTML',
-                    logo: <DiHtml5 />,
+                    logo: <DiHtml5 size='35'  />,
                     alt: 'html'
                 },
                 {
                     id: 2,
                     title: 'CSS',
-                    logo: <DiCss3 />,
+                    logo: <DiCss3 size='35'  />,
                     alt: 'css'
                 },
                 {
                     id: 3,
                     title: 'JavaScript',
-                    logo: <DiJavascript />,
+                    logo: <DiJavascript size='35'  />,
                     alt: 'javascript'
                 },
                 {
                     id: 4,
                     title: 'React',
-                    logo: <DiReact />,
+                    logo: <DiReact size='35'  />,
                     alt: 'react'
+                },
+                {
+                    id: 5,
+                    title: 'npm',
+                    logo: <DiNpm size='35' />,
+                    alt: 'npm'
+                },
+                {
+                    id: 6,
+                    title: 'git',
+                    logo: <DiGit size='35' />,
+                    alt: 'git'
+                },
+                {
+                    id: 7,
+                    title: 'GitHub',
+                    logo: <DiGithubBadge size='35' />,
+                    alt: 'GitHub'
+                },
+                {
+                    id: 8,
+                    title: 'node',
+                    logo: <DiNodejs size='35' />,
+                    alt: 'node'
                 }
             ]
         }
@@ -64,7 +88,7 @@ class ProjectSkillList extends React.Component {
     render() {
         return (
             <Row>
-                <Col xs={12} md={4} className="border-right pl-5 pr-5">
+                <Col xs={12} md={4} className="border-right pl-5 pr-5 d-flex">
                     <div>
                         <h1>Skills</h1>
                         <ul>
